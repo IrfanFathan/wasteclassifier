48c4d5345dfd056a9455eea08321dc5d9a174c0d
Irfan Fathan M <muhammadirfanvnb@gmail.com>
2026-03-14 19:23:13 +0530

    feat: Update app title and manifest for TrashSense branding

--- linux/runner/my_application.cc
+++ linux/runner/my_application.cc
@@ -45,11 +45,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "wasteclassifier");
+    gtk_header_bar_set_title(header_bar, "TrashSense");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "wasteclassifier");
+    gtk_window_set_title(window, "TrashSense");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
